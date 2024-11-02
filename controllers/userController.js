f101498a39c27a2c0ce90287b459677e11161c17
seekolugopi@gmail.com <seekolugopi@gmail.com>
2024-11-02 16:43:12 +0530

    add 23 commit

--- controllers/userController.js
+++ controllers/userController.js
@@ -99,15 +99,11 @@ const searchUsers=async(request,response)=>{
     const {searchText}=request.query 
     console.log(searchText)
     try{
-        let users;
-        if (!searchText){
-            users=undefined;
-        }   
-        users=await userModel.find({$or:[{username:{$regex:searchText, $options:"i"}},{email:{$regex:searchText, $options:"i"}}]})
+        const users=await userModel.find({$or:[{username:{$regex:searchText, $options:"i"}},{email:{$regex:searchText, $options:"i"}}]})
        .populate("posts")
         response.send({users:users,status:200})
     }catch(error){
-        response.send({error_msg:`Internal server Error is ${error}`,status:500})
+        response.send({err_msg:error,status:500})
     }
 }
 
